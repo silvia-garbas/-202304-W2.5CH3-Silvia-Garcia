06ed66a5cd60d12ded46568202e95fa61227ad8c
Silvia <silvia.garbas@gmail.com>
2023-05-10 20:52:19 +0200

    Modified cominlint to .cjs

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,1 +1,21 @@
-const 
+const arrayNumbers = (initialNumber: number, finalNumber: number) => {
+  let newArrayNumbers = [];
+  for (let index = initialNumber; index <= finalNumber; index++) {
+    newArrayNumbers.push(index);
+
+    if (index % 3 === 0 && index % 5 === 0) {
+      newArrayNumbers.pop();
+      newArrayNumbers.push("FizzBuzz");
+    }
+    if (index % 3 === 0) {
+      newArrayNumbers.pop();
+      newArrayNumbers.push("Fizz");
+    }
+    if (index % 5 === 0) {
+      newArrayNumbers.pop();
+      newArrayNumbers.push("Buzz");
+    }
+  }
+  return newArrayNumbers;
+};
+
